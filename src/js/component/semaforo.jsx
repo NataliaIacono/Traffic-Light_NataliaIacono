1a1da196213f563e3b8a6a0957e5274496f3739a
Natalia Iacono <natalia.iacono.pr@gmail.com>
2024-08-05 20:30:45 +0200

    boton cambiar luz ok

--- src/js/component/semaforo.jsx
+++ src/js/component/semaforo.jsx
@@ -3,6 +3,16 @@ import React, { useState } from 'react';
 const Semaforo = () => {
     const [color, setColor] = useState('red');
 
+    const cambiarLuz = () => {
+        if (color === 'red') {
+            setColor('yellow');
+        } else if (color === 'yellow') {
+            setColor('green');
+        } else {
+            setColor('red');
+        }
+    };
+
     return (
         <div className="constainer">
             <div className="palo"></div>
@@ -10,6 +20,9 @@ const Semaforo = () => {
                 <button className={`red ${color === "red" ? "destello" : ""}`} onClick={() => { setColor('red'); }}></button>
                 <button className={`yellow ${color === "yellow" ? "destello" : ""}`} onClick={() => { setColor('yellow'); }}></button>
                 <button className={`green ${color === "green" ? "destello" : ""}`} onClick={() => { setColor('green'); }}></button>
+            </div>
+            <div className='boton contenedor'>
+                <button type="button" className="btn btn-secondary" onClick={cambiarLuz}>Cambia la luz</button>
             </div>
         </div>
     );

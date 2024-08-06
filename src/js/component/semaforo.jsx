bf6225798a75163a1f449fc4a963ad32d6977f0c
Natalia Iacono <natalia.iacono.pr@gmail.com>
2024-08-06 12:34:01 +0200

    Luz morada agregar/quitar

--- src/js/component/semaforo.jsx
+++ src/js/component/semaforo.jsx
@@ -2,16 +2,39 @@ import React, { useState } from 'react';
 
 const Semaforo = () => {
     const [color, setColor] = useState('red');
+    const [morado, setMorado] = useState(false);
+
 
     const cambiarLuz = () => {
-        if (color === 'red') {
-            setColor('yellow');
-        } else if (color === 'yellow') {
-            setColor('green');
+        if (morado) {
+            if (color === "red") {
+                setColor("yellow");
+            } if (color === "yellow") {
+                setColor("green");
+            } if (color === "green") {
+                setColor("purple")
+            } if (color === "purple") {
+                setColor("red")
+            }
         } else {
-            setColor('red');
+            if (color === "red") {
+                setColor("yellow");
+            } if (color === "yellow") {
+                setColor("green");
+            } if (color === "green") {
+                setColor("red")
+            }
         }
+
+        //else if (morado == true) {
+        //   setColor("purple");
+    }
+
+
+    const agregarMorado = () => {
+        setMorado(!morado);
     };
+
 
     return (
         <div className="constainer">
@@ -20,9 +43,13 @@ const Semaforo = () => {
                 <button className={`red ${color === "red" ? "destello" : ""}`} onClick={() => { setColor('red'); }}></button>
                 <button className={`yellow ${color === "yellow" ? "destello" : ""}`} onClick={() => { setColor('yellow'); }}></button>
                 <button className={`green ${color === "green" ? "destello" : ""}`} onClick={() => { setColor('green'); }}></button>
+                {morado && (
+                    <button className={`purple ${color === "purple" ? "destello" : ""}`} onClick={() => { setColor('purple'); }}></button>
+                )}
             </div>
             <div className='boton contenedor'>
                 <button type="button" className="btn btn-secondary" onClick={cambiarLuz}>Cambia la luz</button>
+                <button type="button" className="btn btn-secondary" onClick={agregarMorado}>Luz Morada</button>
             </div>
         </div>
     );
